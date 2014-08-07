17ef9cc748a0b441c5196b7d5d67bd298f964140
Sherah Smith <ssmith@wikimedia.org>
2014-08-07 12:07:46 +0100

    fix question display

--- wikigrok-proto.js
+++ wikigrok-proto.js
@@ -1,18 +1,42 @@
 if (Meteor.isClient) {
+  var gameAnswered = false;
 
   Template.gamePrompt.events({
+
     'click .answerButtons li': function (event) {
-      // template data, if any, is available in 'this'
-      Template.gamePrompt.gameAnswered = true;
+      var answer = event.target.id;
+
       removeGamePrompt();
+      Template.gamePrompt.gameAnswered = true;
       UI.insert(UI.render(Template.gamePrompt), document.body);
-      console.log(Template.gamePrompt);
+
+    },
+
+    'click .xOut': function () {
+      removeGamePrompt();
     }
+
   });
 
   var removeGamePrompt = function(){
+
     $('.gamePrompt').remove();
+
   };
+
+  Template.gamePrompt.helpers({
+    question: function(){
+      console.log(Template.gamePrompt.gameAnswered);
+      var questions = [
+        "Is Paul Rand a Graphic Designer?",
+        "Did Paul Rand win any awards?",
+        "Did Paul Rand go to Pratt Institute?",
+        "Did Paul Rand work for IBM?"
+      ];
+      return questions[parseInt(Math.random() * questions.length)];
+    },
+    gameAnswered: false
+  });
 
 
 
